136a874f2678c7ba4164cc36e80ebd523f0f1d50
Nikitha0320 <nikithareddygujjuri@gmail.com>
2026-03-11 12:10:47 +0530

    updated

--- src/prediction.py
+++ src/prediction.py
@@ -6,9 +6,13 @@ import numpy as np
 class Insurance_Prediction:
     def __init__(self):
         with open("C:\\tekworks\\Projects\\Insurance_prediction\\artifacts\\scaler.pkl","rb") as f:
-            self.scaler=pickle.load(f)
+            import os
+            base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+            scaler_path = os.path.join(base_dir, "artifacts", "scaler.pkl")
+            self.scaler = pickle.load(f)
         with open("C:\\tekworks\\Projects\\Insurance_prediction\\artifacts\\model.pkl","rb") as f:
-            self.model=pickle.load(f)
+            model_path = os.path.join(base_dir, "artifacts", "model.pkl")
+            self.model = pickle.load(f)
     def prediction(self,Age,Annual_Income_LPA,Policy_Term_Years,Sum_Assured_Lakhs):
         input=np.array([[Age,Annual_Income_LPA,Policy_Term_Years,Sum_Assured_Lakhs]])
         scaled_input=self.scaler.transform(input)
